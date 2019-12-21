c71bfa679eb804d669eb5367570fd823302f57e5
Ebrahim Tahernejad <ebrahimtahernejad@gmail.com>
2019-12-21 04:33:09 +0330

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatDatepickerModulePersian } from 'persian-datepicker';
+import { MatIconModule, MatFormFieldModule, MatButtonModule, MatToolbarModule, MatCheckboxModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDatepickerModulePersian,
+    MatToolbarModule,
+    FormsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
